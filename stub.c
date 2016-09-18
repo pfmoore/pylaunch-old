c71ca770d90fef95e98fc52d6bcf92954043d053
Paul Moore <p.f.moore@gmail.com>
2016-09-18 13:59:57 +0100

    Added the ability to append a zipfile

--- stub.c
+++ stub.c
@@ -6,6 +6,70 @@
 #include <windows.h>
 #include <shlwapi.h>
 
+static char end_cdr_sig[4] = { 0x50, 0x4B, 0x05, 0x06 };
+
+int has_appended_zip(wchar_t *filename)
+{
+    BOOL bFlag;
+    DWORD dwFileSize;
+    HANDLE hMapFile;
+    HANDLE hFile;
+    LPVOID lpMapAddress;
+    const char *end;
+    unsigned int comment_len;
+    int ret = 0;
+
+    hFile = CreateFileW(filename, GENERIC_READ,
+                /* Let everyone else at the file, e.g., antivirus? */
+                FILE_SHARE_READ|FILE_SHARE_WRITE|FILE_SHARE_DELETE,
+                NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (hFile == INVALID_HANDLE_VALUE) {
+        goto exit;
+    }
+
+    dwFileSize = GetFileSize(hFile, NULL);
+    /* Must be at least 22 bytes in size, the minimum size of an end-of-zip record */
+    if (dwFileSize < 22) {
+        goto exit;
+    }
+
+    hMapFile = CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, 0, NULL);
+    if (hMapFile == NULL) {
+        goto exit;
+    }
+
+    lpMapAddress = MapViewOfFile(hMapFile, FILE_MAP_READ, 0, 0, 0);
+    if (lpMapAddress == NULL) {
+        goto exit;
+    }
+
+    end = ((const char *)lpMapAddress) + dwFileSize;
+
+    for (comment_len = 0; comment_len <= 0xFFFF; ++comment_len) {
+        unsigned int sigpos = comment_len + 22;
+        unsigned int clpos = comment_len + 2;
+        unsigned int cl;
+        if (memcmp(end-sigpos, end_cdr_sig, 4) != 0)
+            continue;
+        cl = (end-clpos)[0] + ((end-clpos)[1] << 8);
+        if (cl == comment_len) {
+            ret = 1;
+            break;
+        }
+    }
+
+exit:
+    if (lpMapAddress)
+        bFlag = UnmapViewOfFile(lpMapAddress);
+    if (hMapFile)
+        bFlag = CloseHandle(hMapFile);
+    if (hFile)
+        bFlag = CloseHandle(hFile);
+
+    return ret;
+}
+
 wchar_t *appname()
 {
     static wchar_t exe_name[MAX_PATH];
@@ -30,6 +94,7 @@ wchar_t *appname()
         --end;
     }
 
+    if (has_appended_zip(exe_name)) return exe_name;
     wcscpy(end, gui ? L".pyzw" : L".pyz");
     if (PathFileExistsW(exe_name)) return exe_name;
     wcscpy(end, gui ? L".pyw" : L".py");
