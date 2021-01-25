c199597bfe27d03e8acb9d54a95f3d497915b375
Paul Moore <p.f.moore@gmail.com>
2021-01-25 16:10:10 +0000

    Improve dynamic stub

--- dyn/stub.c
+++ dyn/stub.c
@@ -3,29 +3,195 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <shlwapi.h>
+#include <pathcch.h>
 #include <stdio.h>
 
-#define PYTHON_DLL L"python/python3.dll"
 #define SCRIPT_DIR L"scripts"
+#define PYTHON_DIR L"python\\"
+#define PYTHON_DLL PYTHON_DIR L"python3*.dll"
+#define PYTHON_EXE PYTHON_DIR L"python.exe"
 
-Py_Main_t *get_pymain(wchar_t *base_dir) {
-    return 0;
+void error(wchar_t *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    int err = 0;
+    wchar_t *err_msg = 0;
+    if (err) {
+        size_t n = FormatMessageW(
+            FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_ALLOCATE_BUFFER,
+            0,
+            err,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            (wchar_t *)&err_msg,
+            0,
+            0
+        );
+    }
+#ifdef WINDOWS
+    wchar_t *buf = _alloca(BUFSIZE + n + 10);
+    int ret = vswprintf(buf, BUFSIZE, fmt, args);
+    if (err) {
+        wcscat(buf, L":\n");
+        wcscat(buf, err_msg);
+        LocalFree(err_msg);
+    }
+    MessageBoxW(0, buf, L"Python script launcher", MB_OK);
+#else
+    vfwprintf(stderr, fmt, args);
+    if (err) {
+        fwprintf(stderr, L"\n%ls", err_msg);
+        LocalFree(err_msg);
+    }
+    fputc('\n', stderr);
+#endif
+    va_end(args);
+    exit(1);
 }
 
-wchar_t *get_script(wchar_t *base_dir, wchar_t *script_name) {
-    /* Try .pyz, .py in scripts dir, then base dir */
+wchar_t *search_python_dll(wchar_t *dirname, wchar_t *pattern) {
+    wchar_t *dll_pattern;
+    HRESULT hr = PathAllocCombine(
+        dirname, pattern,
+        PATHCCH_ALLOW_LONG_PATHS, &dll_pattern
+    );
+    if (hr != S_OK) {
+        error(L"Could not construct Python DLL pattern");
+    }
+    WIN32_FIND_DATAW find_data;
+    fwprintf(stderr, L"DLL pattern: %ls\n", dll_pattern);
+    HANDLE h = FindFirstFileW(dll_pattern, &find_data);
+    if (h == INVALID_HANDLE_VALUE) {
+        if (GetLastError() == ERROR_FILE_NOT_FOUND) {
+            /* Nothing matches */
+            LocalFree(dll_pattern);
+            return 0;
+        }
+        error(L"Could not match DLL filename pattern");
+    }
+
+    /* Re-use dll_pattern as the directory the DLL is in.
+     * Note that the pattern may have been something like
+     * "python/python*.dll", so we can't just use dirname.
+     */
+    /* NOTE: This doesn't handle forward slashes in filenames! */
+    PathCchRemoveFileSpec(dll_pattern, wcslen(dll_pattern) + 1);
+    fwprintf(stderr, L"DLL pattern: %ls\n", dll_pattern);
+    do {
+        /* Skip the stable ABI DLL, as we can't load symbols from it.
+         * See https://bugs.python.org/issue43022 for details.
+         */
+        if (_wcsicmp(find_data.cFileName, L"python3.dll") == 0) {
+            continue;
+        }
+
+        wchar_t *dll_name;
+        HRESULT hr = PathAllocCombine(dll_pattern, find_data.cFileName, PATHCCH_ALLOW_LONG_PATHS, &dll_name);
+        if (hr != S_OK) {
+            error(L"Could not construct Python DLL name");
+        }
+        fwprintf(stderr, L"Trying DLL name %ls\n", dll_name);
+        return dll_name;
+    } while (FindNextFileW(h, &find_data));
+    LocalFree(dll_pattern);
+    if (GetLastError() != ERROR_NO_MORE_FILES) {
+        error(L"Search for Python DLL failed");
+    }
+    FindClose(h);
     return 0;
 }
 
 typedef int (*Py_Main_t)(int argc, wchar_t **argv);
 
-static char end_cdr_sig[4] = { 0x50, 0x4B, 0x05, 0x06 };
+Py_Main_t get_pymain(wchar_t *base_dir) {
+    wchar_t *dll_path = search_python_dll(base_dir, PYTHON_DLL);
+    if (dll_path == 0) {
+        error(L"Could not find Python DLL");
+    }
 
-void error(wchar_t *err)
-{
-    fwprintf(stderr, err);
-    exit(1);
+    HMODULE py_dll = LoadLibraryW(dll_path);
+    if (!py_dll) {
+        error(L"Could not load Python DLL %ls", dll_path);
+    }
+    LocalFree(dll_path);
+
+    Py_Main_t py_main = (Py_Main_t)GetProcAddress(py_dll, "Py_Main");
+    if (!py_main) {
+        error(L"Could not locate Py_Main function");
+    }
+
+    return py_main;
 }
+
+wchar_t *get_python_exe(wchar_t *base_dir) {
+    wchar_t *exe_path;
+    HRESULT hr = PathAllocCombine(
+        base_dir, PYTHON_EXE,
+        PATHCCH_ALLOW_LONG_PATHS, &exe_path
+    );
+    if (hr != S_OK) {
+        error(L"Could not construct Python executable path");
+    }
+    /* Caller should free using LocalFree, but in practice
+     * we don't do so, as we're just transferring control
+     * to the script and then exiting, so process cleanup
+     * will sort it out.
+     */
+    return exe_path;
+}
+
+wchar_t *get_script(wchar_t *base_dir, wchar_t *script_name) {
+    /* Add 10 here for two / characters, an extra w in the extension, plus a bit of padding. */
+    size_t script_size = wcslen(base_dir) + wcslen(SCRIPT_DIR) + wcslen(script_name) + 10;
+    wchar_t *script = malloc(script_size * sizeof(wchar_t));
+    if (script == 0) {
+        error(L"Could not allocate memory for script name");
+    }
+
+    /* Try .pyz, .py in base dir, then scripts dir */
+    const wchar_t *directories[] = {L"", SCRIPT_DIR, 0};
+    const wchar_t *extensions[] = {
+#ifdef WINDOWS
+        L".pyzw", L".pyw", 0
+#else
+        L".pyz", L".py", 0
+#endif
+    };
+
+    /* Build the name bit by bit, and then check it */
+    wcscpy(script, base_dir);
+    size_t base_len = wcslen(script);
+
+    for (const wchar_t **dir = directories; *dir; ++dir) {
+        if ((*dir)[0]) {
+            HRESULT hr = PathCchCombineEx(script, script_size, script, SCRIPT_DIR, PATHCCH_ALLOW_LONG_PATHS);
+            if (hr != S_OK) {
+                error(L"Cannot build name of scripts directory");
+            }
+        }
+        HRESULT hr = PathCchCombineEx(script, script_size, script, script_name, PATHCCH_ALLOW_LONG_PATHS);
+        if (hr != S_OK) {
+            error(L"Cannot build name of scripts directory");
+        }
+        for (const wchar_t **ext = extensions; *ext; ++ext) {
+            HRESULT hr = PathCchRenameExtension(script, script_size, *ext);
+            if (hr != S_OK) {
+                error(L"Cannot set script extension");
+            }
+            /* OK, so does this option exist? */
+            if (PathFileExistsW(script)) {
+                return script;
+            }
+        }
+        /* Reset to the base directory */
+        script[base_len] = 0;
+    }
+
+    /* No joy. Free memory and return failure */
+    free(script);
+    return 0;
+}
+
+static char end_cdr_sig[4] = { 0x50, 0x4B, 0x05, 0x06 };
 
 int has_appended_zip(wchar_t *filename)
 {
@@ -93,72 +259,59 @@ exit:
     return ret;
 }
 
-void exename()
-{
-    static wchar_t exe_name[MAX_PATH];
-    DWORD ret = GetModuleFileNameW(NULL, exe_name, MAX_PATH);
-    wchar_t *app = NULL;
-    if (has_appended_zip(exe_name)) app = _wcsdup(exe_name);
-    wchar_t *leaf = _wcsdup(PathFindFileNameW(exe_name));
-    PathCchRemoveFileSpec(exe_name, MAX_PATH);
-    wchar_t *dll_loc;
-    HRESULT hr = PathAllocCombine(exe_name, L"python/python3.dll", PATHCCH_ALLOW_LONG_PATHS, dll_loc);
-    if (hr != S_OK) {
-        error(L"Oops");
+wchar_t *split_exe_name(wchar_t **script_path) {
+    /* Initialise return values */
+    *script_path = 0;
+
+    size_t exe_bufsize = MAX_PATH+1;
+    wchar_t *exe_name = malloc(exe_bufsize * sizeof(wchar_t));
+    DWORD ret = GetModuleFileNameW(NULL, exe_name, exe_bufsize);
+
+    /* Increase buffer size incrementally to allow for long paths? */
+    while (ret == exe_bufsize) {
+        exe_bufsize *= 2;
+        if (exe_bufsize > 1000000) {
+            error(L"Module filename seems to be too long");
+        }
+        exe_name = realloc(exe_name, exe_bufsize * sizeof(wchar_t));
+        ret = GetModuleFileNameW(NULL, exe_name, exe_bufsize);
     }
-    fwprintf(stderr, L"exe_name %ls, path %ls, dll %ls\n", leaf, exe_name, dll_name);
-    wchar_t *script_dir;
-    hr = PathAllocCombine(exe_name, L"scripts", PATHCCH_ALLOW_LONG_PATHS, script_dir);
-    if (hr != S_OK) {
-        error(L"Oops");
+    if (ret == 0) {
+        error(L"Could not get executable filename");
     }
-    if (!app) {
-        app = appname(script_dir, leaf);
-        if (!app) {
-            app = appname(exe_name, leaf);
+
+    /* First of all, check if we have an appended zipfile */
+    wchar_t *exe_leaf = NULL;
+    if (has_appended_zip(exe_name)) {
+        *script_path = _wcsdup(exe_name);
+        if (*script_path == NULL) {
+            error(L"Out of memory allocating script path");
+        }
+    } else {
+        /* We need to search, so keep the exe basename */
+        exe_leaf = _wcsdup(PathFindFileNameW(exe_name));
+        if (exe_leaf == NULL) {
+            error(L"Out of memory allocating exe leafname");
         }
     }
+
+    /* Note: PathCchRemoveFileSpec only respects backslashes,
+     * but exe_name came from a Windows API that returns
+     * backslashes, so we're OK.
+     */
+    HRESULT hr = PathCchRemoveFileSpec(exe_name, exe_bufsize);
+    if (hr != S_OK) {
+        error(L"Could not get executable directory name");
+    }
+
+    /* If we need to search, do it now */
+    if (exe_leaf) {
+        *script_path = get_script(exe_name, exe_leaf);
+        free(exe_leaf);
+    }
+
+    return exe_name;
 }
-
-wchar_t *appname()
-{
-    static wchar_t exe_name[MAX_PATH];
-    DWORD ret = GetModuleFileNameW(NULL, exe_name, MAX_PATH);
-    wchar_t *end;
-    int gui = 0;
-
-    if (ret == 0) {
-        /* TODO: Use MessageBox for GUI */
-        fwprintf(stderr, L"Could not get executable filename\n");
-        exit(1);
-    }
-
-    end = exe_name + (ret - 4);
-    if (wcsicmp(end, L".exe") != 0) {
-        fwprintf(stderr, L"Executable does not appear to end with .exe\n");
-        exit(1);
-    }
-
-    if (end[-1] == L'w' || end[-1] == L'W') {
-        gui = 1;
-        --end;
-    }
-
-    if (has_appended_zip(exe_name)) return exe_name;
-    wcscpy(end, gui ? L".pyzw" : L".pyz");
-    if (PathFileExistsW(exe_name)) return exe_name;
-    wcscpy(end, gui ? L".pyw" : L".py");
-    if (PathFileExistsW(exe_name)) return exe_name;
-    wcscpy(end, L".zip");
-    if (PathFileExistsW(exe_name)) return exe_name;
-
-    /* Couldn't find script to run */
-    *end = L'\0';
-    fwprintf(stderr, L"Unable to locate script to run: %ls.{pyz%ls,py%ls,zip}\n",
-            exe_name, gui ? L"w" : L"", gui ? L"w" : L"");
-    return 0;
-}
-
 
 #ifdef WINDOWS
 int WINAPI wWinMain(
@@ -171,33 +324,22 @@ int WINAPI wWinMain(
 int wmain()
 #endif
 {
-    HMODULE py_dll;
-    Py_Main_t py_main;
-
-    /* Should locate embedded dist here... */
-    py_dll = LoadLibraryW(L"python3.dll");
-    if (!py_dll) {
-        error(L"Could not load Python DLL");
-        return 1;
-    }
-    py_main = (Py_Main_t)GetProcAddress(py_dll, "Py_Main");
-    if (!py_main) {
-        error(L"Could not locate Py_Main function");
-        return 1;
-    }
-    exename();
-    return 0;
-    wchar_t **myargv = malloc((__argc + 2) * sizeof(wchar_t*));
-    int i;
-    wchar_t *script = appname();
+    wchar_t *script = NULL;
+    wchar_t *base_dir = split_exe_name(&script);
     if (!script) {
-        exit(1);
+        error(L"Could not find a script to execute");
     }
-    myargv[0] = __wargv[0]; /* Python executable - get from embedded dist? */
+
+    wchar_t **myargv = malloc((__argc + 2) * sizeof(wchar_t*));
+    myargv[0] = get_python_exe(base_dir); /* __wargv[0]; Python executable - get from embedded dist? */
     myargv[1] = script;
+
+    int i;
     for (i = 1; i < __argc; ++i) {
         myargv[1+i] = __wargv[i];
     }
     myargv[1+i] = 0;
+
+    Py_Main_t py_main = get_pymain(base_dir);
     return py_main(__argc+1, myargv);
 }
